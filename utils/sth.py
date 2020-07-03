6d0335f2f029a0f2a12628c2b26ac08cc3ea7745
Keavnn <271668153@qq.com>
2020-07-03 09:40:56 +0800

    little improvements.
    1. optimized the way calculating discounted return. 
    2. removed unnecessary functuons.

--- utils/sth.py
+++ utils/sth.py
@@ -1,23 +1,25 @@
 import os
+import itertools
 import numpy as np
 
 
 class sth(object):
+
     @staticmethod
     def discounted_sum(x, gamma, init_value, dones):
         y = []
-        for i in reversed(range(len(x))):
-            init_value = gamma * (1 - dones[i]) * init_value + x[i]
-            y.insert(0, init_value)
-        return y
+        for _x, _d in zip(x[::-1], dones[::-1]):
+            init_value = gamma * (1 - _d) * init_value + _x
+            y.append(init_value)
+        return y[::-1]
 
     @staticmethod
     def discounted_sum_minus(x, gamma, init_value, dones, z):
         y = []
-        for i in reversed(range(len(x))):
-            y.insert(0, gamma * (1 - dones[i]) * init_value + x[i] - z[i])
-            init_value = z[i]
-        return y
+        for _x, _d, _z in zip(x[::-1], dones[::-1], z[::-1]):
+            y.append(gamma * (1 - _d) * init_value + _x - _z)
+            init_value = _z
+        return y[::-1]
 
     @staticmethod
     def int2action_index(x, action_dim_list):
@@ -37,39 +39,9 @@ class sth(object):
             [2 1 0]
             [2 1 1]]
         """
-        y = []
-        x = np.squeeze(x).reshape(-1)
-        for i in reversed(action_dim_list):
-            y.insert(0, x % i)
-            x //= i
-        return np.array(y).T
-
-    @staticmethod
-    def action_index2int(z, action_dim_list):
-        '''
-        input: [[0 0 0]
-                [0 0 1]
-                [0 1 0]
-                [0 1 1]
-                [1 0 0]
-                [1 0 1]
-                [1 1 0]
-                [1 1 1]
-                [2 0 0]
-                [2 0 1]
-                [2 1 0]
-                [2 1 1]], [3, 2, 2]
-        output: [ 0  1  2  3  4  5  6  7  8  9 10 11]
-        '''
-        assert isinstance(z, np.ndarray), 'type of sth.action_index2int.z must be np.ndarray'
-        if len(z.shape) == 1:
-            z = z[np.newaxis, :]
-        x = []
-        y = 1
-        for i in list(reversed(action_dim_list)):
-            x.insert(0, y)
-            y *= i
-        return z.dot(np.array(x))
+        x = np.array(x)
+        index_list = list(itertools.product(*[list(range(l)) for l in action_dim_list]))
+        return np.asarray(index_list)[x]
 
     @staticmethod
     def int2one_hot(x, action_dim_prod):
@@ -89,70 +61,5 @@ class sth(object):
                 [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]]
         '''
         x = np.asarray(x).flatten()
-        if hasattr(x, '__len__'):
-            a = np.zeros([len(x), action_dim_prod])
-            for i in range(len(x)):
-                a[i, x[i]] = 1
-        else:
-            a = np.zeros(action_dim_prod)
-            a[x] = 1
+        a = np.eye(action_dim_prod)[x]
         return a
-
-    @staticmethod
-    def action_index2one_hot(index, action_dim_list):
-        '''
-        input: [[0 0 0]
-                [0 0 1]
-                [0 1 0]
-                [0 1 1]
-                [1 0 0]
-                [1 0 1]
-                [1 1 0]
-                [1 1 1]
-                [2 0 0]
-                [2 0 1]
-                [2 1 0]
-                [2 1 1]], [3, 2, 2]
-        output: [[1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-                [0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-                [0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-                [0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0.]
-                [0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0.]
-                [0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]
-                [0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
-                [0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
-                [0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0.]
-                [0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0.]
-                [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0.]
-                [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]]
-        '''
-        assert isinstance(index, np.ndarray), 'type of sth.action_index2one_hot.index must be np.ndarray'
-        if len(index.shape) == 1:
-            index = index[:, np.newaxis]
-        return sth.int2one_hot(sth.action_index2int(index, action_dim_list), np.asarray(action_dim_list).prod())
-
-    @staticmethod
-    def get_batch_one_hot(action, action_multiplication_factor, cols):
-        """
-        input: [[2, 1],[2, 0]], [3, 1], 9
-        output: [[0, 0, 0, 0, 0, 0, 0, 1, 0],
-                 [0, 0, 0, 0, 0, 0, 1, 0, 0]]
-        """
-        assert isinstance(action, np.ndarray), 'type of sth.get_batch_one_hot.action must be np.ndarray'
-        assert isinstance(action_multiplication_factor, np.ndarray), 'type of sth.get_batch_one_hot.action_multiplication_factor must be np.ndarray'
-        ints = action.dot(action_multiplication_factor)
-        x = np.zeros([action.shape[0], cols])
-        for i, j in enumerate(ints):
-            x[i, j] = 1
-        return x
-
-    @staticmethod
-    def action_index2action_value(action_index, action_dim_list):
-        """
-        let actions' value between -1 and 1, if action_lict is [3,3], means that every dimension has 3 actions average from -1 to 1, like [-1, 0, 1], so index [0, 2] means action value [-1, 1]
-        input: [0, 2], [3, 3]
-        output: [-1, 1]
-        """
-        assert isinstance(action, np.ndarray), 'type of sth.action_index2action_value.action must be np.ndarray'
-        assert 1 not in action_dim_list, 'sth.action_index2action_value.action_dim_list must not include 1'
-        return 2 / (np.array([action_dim_list]) - 1) * action_index - 1
